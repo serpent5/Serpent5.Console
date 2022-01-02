4472022a7c090180624d9604966d2668dee4b6a2
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2022-01-02 20:11:19 +0000

    Initial Commit.

---
+++ Serpent5.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿#pragma warning disable  // Avoid uninstantiated internal classes
+
+using Console = System.Console;
+
+Console.WriteLine("Hello, World.");
+
+#pragma warning restore  // Avoid uninstantiated internal classes
